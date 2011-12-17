4ac636e417009ab12c0bd161245c30e826a52f7f
Reik Val <guardian_de_las_sombras@hotmail.com>
2011-12-17 19:31:41 +0100

    Task class

---
+++ Task.h
@@ -0,0 +1,14 @@
+#ifndef CLASS_TASK_H
+#define CLASS_TASK_H
+
+#include "Globals.h"
+
+class Task {
+      public:
+             Task();
+             ~Task();
+      private:
+              
+};
+
+#endif
