b440d85bd9759f6972760ed671b89dae02a32831
Reik Val <guardian_de_las_sombras@hotmail.com>
2011-12-19 22:30:58 +0100

    Funcion player, condiciones de derrota

--- src/Worker.cpp
+++ src/Worker.cpp
@@ -6,27 +6,27 @@ Worker::Worker() : Sprite("assets/worker.xml")
   happiness = 0; //Number of life wich will change in the next turn
   hours = 0; //Hours 0 => No time to the current task
   sueldo = 0.2; //0.2 => 20% of the money go to the Worker
-  resource_gathered = 0;
-  resource_exported = 0;
+  resource = 0;
 }
 
 Worker::~Worker() {
  
 }
 
+int Worker::getLife() {
+  return life;
+}
+
 void Worker::changeLife(int inc) {
   life += inc;
 }
 
+int Worker::getTime() {
+  return hours;    
+}
 void Worker::changeHours(int h) {
   hours = h;
 }
-void Worker::changeResource(bool isGathered, Resource *res) {
-  if(isGathered) {
-    resource_gathered = res;
-    resource_exported = NULL;
-  } else {
-    resource_gathered = NULL;
-    resource_exported = res;
-  }
+void Worker::changeResource(Resource *res) {
+    resource = res;
 }
